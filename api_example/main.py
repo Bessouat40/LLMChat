2038c4ff3bef8ae9c00f336e84abe7123ccddab6
Bessouat40 <roman.bessouat@orange.fr>
2025-01-23 21:16:36 +0100

    add raglight update

--- api_example/main.py
+++ api_example/main.py
@@ -1,32 +1,25 @@
-import warnings
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-import os
-
 from pydantic import BaseModel
 
-from raglight.rag.builder import Builder
+from raglight.rag.simple_rag_api import RAGPipeline
+from raglight.models.data_source_model import FolderSource, GitHubSource
 from raglight.config.settings import Settings
-from dotenv import load_dotenv
 
-load_dotenv()
 
 class QueryModel(BaseModel):
     question: str
 
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
-model_embeddings = os.environ.get('MODEL_EMBEDDINGS')
-model_name = os.environ.get('MODEL_NAME')
-system_prompt_directory = os.environ.get('SYSTEM_PROMPT_DIRECTORY')
-collection_name = os.environ.get('COLLECTION_NAME')
+Settings.setup_logging()
 
-rag = Builder() \
-.with_embeddings(Settings.HUGGINGFACE, model_name=model_embeddings) \
-.with_vector_store(Settings.CHROMA, persist_directory=persist_directory, collection_name=collection_name) \
-.with_llm(Settings.OLLAMA, model_name=model_name, system_prompt_file=system_prompt_directory) \
-.build_rag()
+pipeline = RAGPipeline(knowledge_base=[
+GitHubSource(url="https://github.com/Bessouat40/RAGLight")
+# FolderSource(path="Path to your knowledge base containing PDF, docx, ...")
+], model_name="llama3")
+
+pipeline.build()
 
 app = FastAPI()
 
@@ -40,7 +33,7 @@ app.add_middleware(
 
 @app.post("/newQuestion")
 async def get_images(question: QueryModel):
-    response = rag.question_graph(question.question)
+    response = pipeline.generate(question.question)
     return {"response": response}
 
 if __name__ == "__main__":
